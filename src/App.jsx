9e2a79c7f73aabc4a10c3535466acdc093cf54fc
hypercube-integration <hypercube-integration@outlook.com>
2025-08-07 21:10:01 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Dashboard from './pages/Dashboard';
+import './styles.css';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
